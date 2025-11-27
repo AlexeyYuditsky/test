cd3b25acf633f87cbc5c7a49f9b410177cb1d649
AlexeyYuditsky <alexeyyuditsky@gmail.com>
2025-11-27 09:53:38 +0300

    test

--- app/src/main/java/com/alexeyyuditsky/test/Test3.kt
+++ app/src/main/java/com/alexeyyuditsky/test/Test3.kt
@@ -1,20 +1,13 @@
 package com.alexeyyuditsky.test
 
 val seq = sequence {
-    println("Generating first")
+    println("start")
     yield(1)
-    println("Generating second")
-    yield(2)
-    println("Generating third")
-    yield(3)
-    println("Done")
+    println("end")
 }
 
 fun main() {
     val iterator = seq.iterator()
-    println("Starting")
-    val first = iterator.next()
-    println("First: $first")
-    val second = iterator.next()
-    println("Second: $second")
+    println(iterator.next())
+    println(iterator.next())
 }
